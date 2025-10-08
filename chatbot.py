b5dc4162dbcb4ddcff662e9201280f417f2c51a3
Franco Pena-Lepe <fpena-lepe@cps.edu>
2025-10-08 11:21:27 -0500

    added basic function

--- chatbot.py
+++ chatbot.py
@@ -2,3 +2,17 @@ from games import game_db
 from match import match
 from typing import List, Tuple, Callable, Any
 
+def get_title(game: Tuple[str, str, int, List[str]]) -> str:
+    return game[0]
+
+
+def get_director(game: Tuple[str, str, int, List[str]]) -> str:
+    return game[1]
+
+
+def get_year(game: Tuple[str, str, int, List[str]]) -> int:
+    return game[2]
+
+
+def get_actors(game: Tuple[str, str, int, List[str]]) -> List[str]:
+    return game[3]
