8cb39ac8c417cce91c4cd115d7f10d6ec352c1b8
Franco Pena-Lepe <fpena-lepe@cps.edu>
2025-10-08 11:15:12 -0500

    added database and skeleton

---
+++ chatbot.py
@@ -0,0 +1,4 @@
+from games import game_db
+from match import match
+from typing import List, Tuple, Callable, Any
+
---
+++ games.py
@@ -0,0 +1,24 @@
+from typing import List, Tuple
+
+game_db: List[Tuple[str, str, int, List[str]]] = [
+    ("the legend of zelda: breath of the wild", "nintendo", 2017, ["link", "zelda", "ganon"]),
+    ("the witcher 3: wild hunt", "cd projekt red", 2015, ["geralt", "yennefer", "triss"]),
+    ("god of war", "santa monica studio", 2018, ["kratos", "atreus"]),
+    ("hollow knight", "team cherry", 2017, ["knight", "hornet"]),
+    ("elden ring", "fromsoftware", 2022, ["tarnished", "melina", "radahn"]),
+    ("red dead redemption 2", "rockstar games", 2018, ["arthur morgan", "dutch", "john marston"]),
+    ("the last of us", "naughty dog", 2013, ["joel", "ellie"]),
+    ("cyberpunk 2077", "cd projekt red", 2020, ["v", "johnny silverhand"]),
+    ("portal 2", "valve", 2011, ["chell", "glados", "wheatley"]),
+    ("minecraft", "mojang", 2011, ["steve", "alex"]),
+    ("super mario odyssey", "nintendo", 2017, ["mario", "cappy", "bowser"]),
+    ("metal gear solid v: the phantom pain", "konami", 2015, ["big boss", "quiet", "ocelot"]),
+    ("persona 5", "atlus", 2016, ["joker", "morgana", "ann", "ryuji"]),
+    ("bioshock infinite", "irrational games", 2013, ["booker dewitt", "elizabeth"]),
+    ("undertale", "toby fox", 2015, ["frisk", "sans", "papyrus", "toriel"]),
+    ("mass effect 2", "bioware", 2010, ["commander shepard", "garrus", "liara"]),
+    ("half-life 2", "valve", 2004, ["gordon freeman", "alyx vance"]),
+    ("doom eternal", "id software", 2020, ["doom slayer", "vega", "khan maykr"]),
+    ("final fantasy vii", "square enix", 1997, ["cloud", "tifa", "aerith", "sephiroth"]),
+    ("stardew valley", "concernedape", 2016, ["farmer", "sebastian", "abigail", "penny"]),
+]
